f7e53cc86f83a8d04659cc4f1cbb4d97933868db
AleehSophia <alessia.casado@gmail.com>
2022-11-01 17:05:32 -0300

    :tada: Initial setup

---
+++ src/main/java/com/educandoweb/springpedidos/SpringPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/springpedidos/SpringPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
