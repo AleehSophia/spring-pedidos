48e8be0d3a50587ebe02b91eb212e41ec811ad1e
AleehSophia <alessia.casado@gmail.com>
2022-11-02 17:13:06 -0300

    feat: association ManyToMany between category and product

--- src/main/java/com/educandoweb/springpedidos/entities/Category.java
+++ src/main/java/com/educandoweb/springpedidos/entities/Category.java
@@ -1,5 +1,6 @@
 package com.educandoweb.springpedidos.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,7 +15,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {}
