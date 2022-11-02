53f0db30623bd5562eafa1a1bff92ad2ead2f794
AleehSophia <alessia.casado@gmail.com>
2022-11-02 09:43:10 -0300

    mapping entity

--- src/main/java/com/educandoweb/springpedidos/entities/User.java
+++ src/main/java/com/educandoweb/springpedidos/entities/User.java
@@ -1,10 +1,16 @@
 package com.educandoweb.springpedidos.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
