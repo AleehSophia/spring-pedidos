325ef5501d50a579b67895e3cec6f42f5c506709
AleehSophia <alessia.casado@gmail.com>
2022-11-03 12:29:00 -0300

    Product-OrderItem OneToMany association

--- src/main/java/com/educandoweb/springpedidos/entities/OrderItem.java
+++ src/main/java/com/educandoweb/springpedidos/entities/OrderItem.java
@@ -38,6 +38,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
+    @JsonIgnore
     public Product getProduct() {
         return id.getProduct();
     }
--- src/main/java/com/educandoweb/springpedidos/entities/Product.java
+++ src/main/java/com/educandoweb/springpedidos/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoweb.springpedidos.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,6 +25,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {}
 
@@ -77,6 +81,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
