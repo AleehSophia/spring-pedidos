815c149e11f3238d18aca5115071a594849a24e3
AleehSophia <alessia.casado@gmail.com>
2022-11-07 13:00:55 -0300

    :goal_net:exception handling delete

--- src/main/java/com/educandoweb/springpedidos/controllers/exceptions/ResouceExceptionError.java
+++ src/main/java/com/educandoweb/springpedidos/controllers/exceptions/ResouceExceptionError.java
@@ -1,5 +1,6 @@
 package com.educandoweb.springpedidos.controllers.exceptions;
 
+import com.educandoweb.springpedidos.services.exceptions.DatabaseException;
 import com.educandoweb.springpedidos.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,15 @@ public class ResouceExceptionError {
 
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
     }
---
+++ src/main/java/com/educandoweb/springpedidos/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.educandoweb.springpedidos.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
