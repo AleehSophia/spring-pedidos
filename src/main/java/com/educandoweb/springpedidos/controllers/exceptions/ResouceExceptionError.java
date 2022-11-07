3b7251303ad0d91dff120fccf90c52144ffdcf3a
AleehSophia <alessia.casado@gmail.com>
2022-11-07 12:15:30 -0300

    :goal_net:Implementing exception User findById

---
+++ src/main/java/com/educandoweb/springpedidos/controllers/exceptions/ResouceExceptionError.java
@@ -0,0 +1,23 @@
+package com.educandoweb.springpedidos.controllers.exceptions;
+
+import com.educandoweb.springpedidos.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResouceExceptionError {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+}
